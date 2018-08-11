0944f84eb8c22d962f229c0a3b48a5eb7a67b39d
Xizhou Li <xl006@bucknell.edu>
2018-08-11 12:08:59 -0700

    minor changes

--- utility/locationGenerate.js
+++ utility/locationGenerate.js
@@ -2,8 +2,22 @@
 var longitudes = [];
 var latitudes = [];
 
-for(var i = 0; i < 100000; i++){
-	var longitude = Math.random() * 181 - 90;
-	var latitude = Math.random() * 361 - 180;
+const googleApiMapToken = require('./googleMapApi.js');
+const request = require('request');
+// console.log($);
+const locations = [];
+for(let i = 0; i < 2000; i++){
+	const longitude = Math.random() * 181 - 90;
+	const latitude = Math.random() * 361 - 180;
+	const combine = `${longitude},${latitude}`;
+	console.log(combine);
+	request({
+		url: `https://maps.googleapis.com/maps/api/geocode/json?latlng=${longitude},${latitude}&key=${googleApiMapToken}`,
+		method: 'get'
+	}, (err, res, body) => {
+		locations.push(res);
+	});
 
 }
+
+//https://maps.googleapis.com/maps/api/geocode/json?latlng=40.714224,-73.961452&key=YOUR_API_KEY
